63fbe0367e9674659236442d72aa132dfec56d1f
pathum7949 <pathum7949@gmail.com>
2024-02-09 14:15:04 +0530

    Dog function added

--- animal.py
+++ animal.py
@@ -2,8 +2,14 @@ import sys
 def default():
     print('Hello')
 
+def dog():
+    print("Baw")
+
 def main():
-    default()
+    if sys.argv[1] == 'dog':
+        dog()
+    else:
+        default()
 
 if __name__ == '__main__':
     main()
